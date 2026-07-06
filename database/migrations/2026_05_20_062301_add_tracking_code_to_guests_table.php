8537860c4434097b1ebdf6167f41940e38c3577b
BlackSuede9 <alifazizy9h03@gmail.com>
2026-07-06 10:29:12 +0700

    visitor

--- database/migrations/2026_05_20_062301_add_tracking_code_to_guests_table.php
+++ database/migrations/2026_05_20_062301_add_tracking_code_to_guests_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('daftar_tamu', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             
                 $table->string('tracking_code')
                     ->unique()
@@ -24,7 +24,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('daftar_tamu', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             //
         });
     }
